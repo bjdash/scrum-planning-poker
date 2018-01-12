c13f359475f4e909dd45549f631bb947fc9ebf03
Bijay <bijaya.k.dash@gmail.com>
2018-01-13 02:02:48 +0530

    typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var bodyParser = require('body-parser');
 
